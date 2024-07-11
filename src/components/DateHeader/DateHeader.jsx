e5590461818ceb93e7ab6dec496c93e6494ec760
Boldizsar Szabo <szboldisz@gmail.com>
2024-07-11 09:18:41 +0200

    Modify DateHeader component

--- src/components/DateHeader/DateHeader.jsx
+++ src/components/DateHeader/DateHeader.jsx
@@ -1,31 +1,46 @@
 import styles from "./DateHeader.module.css";
 
 function formatDate(date) {
+  if (!date) return { day: null, month: null };
+
   const dateObj = new Date(date);
   const day = dateObj.getDate();
   const month = dateObj.toLocaleDateString("en-US", { month: "short" });
+
   return { day, month };
 }
 
 export default function DateHeader({ date }) {
-  const { day: dayStart, month: monthStart } = formatDate(date.start);
-  const { day: dayEnd, month: monthEnd } = formatDate(date.end);
+  const { day: dayStart, month: monthStart } = formatDate(date?.start);
+  const { day: dayEnd, month: monthEnd } = formatDate(date?.end);
+
+  if (!date) {
+    return (
+      <div className={styles.date}>
+        <span className={styles.day}>TBD</span>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.date}>
-      <div className={styles.dateStart}>
-        <span className={styles.month}>{monthStart}</span>
-        <span className={styles.day}>{dayStart}</span>
-      </div>
+      {dayStart && monthStart && (
+        <div className={styles.dateStart}>
+          <span className={styles.month}>{monthStart}</span>
+          <span className={styles.day}>{dayStart}</span>
+        </div>
+      )}
 
-      <span className={styles.separator}>-</span>
+      {dayStart && dayEnd && <span className={styles.separator}>-</span>}
 
-      <div className={styles.dateEnd}>
-        {monthStart !== monthEnd && (
-          <span className={styles.month}>{monthEnd}</span>
-        )}
-        <span className={styles.day}>{dayEnd}</span>
-      </div>
+      {dayEnd && monthEnd && (
+        <div className={styles.dateEnd}>
+          {monthStart !== monthEnd && (
+            <span className={styles.month}>{monthEnd}</span>
+          )}
+          <span className={styles.day}>{dayEnd}</span>
+        </div>
+      )}
     </div>
   );
 }
