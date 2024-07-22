25a4f82ddf7e2f6d523c521db358899ef45f649b
Boldizsar Szabo <szboldisz@gmail.com>
2024-07-22 12:08:04 +0200

    Update btn component

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -19,24 +19,12 @@ const Btn = styled.button`
     background-color: ${({ theme }) => theme.colors.accent[0]};
   }
 
-  &:active {
-    ${"" /* box-shadow: 0 0 0.2rem rgba(0, 0, 0, 0.1); */}
-  }
-
   &:disabled,
   &:disabled:hover,
   &:disabled:active {
     cursor: not-allowed;
     opacity: 0.55;
-    box-shadow: 0 0 0.2rem rgba(0, 0, 0, 0.1);
-    transform: scale(1);
   }
 `;
-
-Btn.defaultProps = {
-  color: `${({ theme }) => theme.colors.text[0]}`,
-  bgColor: `${({ theme }) => theme.colors.bg[0]}`,
-  borderColor: `${({ theme }) => theme.colors.bg[0]}`,
-};
 
 export default Btn;
