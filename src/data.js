e3c92285e7ff391b5844812b8b39f90fa4293092
Boldizsar Szabo <szboldisz@gmail.com>
2024-07-25 08:38:03 +0200

    Modify series data

--- src/data.js
+++ src/data.js
@@ -3,15 +3,17 @@ export const defaultFilterValues = {
 };
 
 export const seriesData = [
+  "Champions of the Future",
+  "FIA Endurance Championship",
   "FIA European Championship",
   "FIA World Championship",
-  "IAME Euro",
+  "IAME Euro Series",
   "IAME Warrior Final",
   "Moravský Pohár",
   "RMC Austria",
   "RMC BNL",
   "RMC CEE",
-  "RMC Euro",
+  "RMC Euro Trophy",
   "RMC Germany",
   "RMC Grand Finals",
   "RMC Hungary",
