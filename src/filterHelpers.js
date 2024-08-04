aa750c0e944f564e4b7061513bb1fa3053c89f2d
Boldizsar Szabo <szboldisz@gmail.com>
2024-08-04 12:37:33 +0200

    Refactor code

--- src/filterHelpers.js
+++ src/filterHelpers.js
@@ -8,7 +8,7 @@ function createInitialFilters(filterKeys) {
   return initialFilters;
 }
 
-//   Extract filter options and return them as an object
+// Extract filter options and return them as an object
 function extractFilterOptions(fetchedData) {
   const sorting = ["Date ascending", "Date descending"];
 
@@ -47,105 +47,126 @@ function extractFilterOptions(fetchedData) {
   };
 }
 
+// Takes 'races' (array of races (objects)) and 'sorting' (string containing 'ascending' or 'descending')
+function sortRaces(races, sorting) {
+  if (sorting.includes("ascending")) {
+    return races.sort((a, b) => new Date(a.end_date) - new Date(b.end_date));
+  } else if (sorting.includes("descending")) {
+    return races.sort((a, b) => new Date(b.end_date) - new Date(a.end_date));
+  }
+
+  return races;
+}
+
+// Takes 'races' (array of races (objects)) and 'status' (string equal to 'Upcoming', 'Ongoing' or 'Finished')
+function filterByStatus(races, status) {
+  const currentDate = new Date();
+
+  if (status.toLowerCase() === "upcoming") {
+    return races.filter((race) => new Date(race.end_date) > currentDate);
+  } else if (status.toLowerCase() === "ongoing") {
+    return races.filter(
+      (race) =>
+        new Date(race.start_date === null ? race.end_date : race.start_date) <=
+          currentDate && new Date(race.end_date) >= currentDate
+    );
+  } else if (status.toLowerCase() === "finished") {
+    return races.filter((race) => new Date(race.end_date) < currentDate);
+  }
+
+  return races;
+}
+
+// Takes 'races' (array of races (objects)) and 'years' (array of years (strings))
+function filterByYears(races, years) {
+  return races.filter((race) => years.includes(race.end_date.slice(0, 4)));
+}
+
+// Takes 'races' (array of races (objects)) and 'months' (array of months (strings))
+function filterByMonths(races, months) {
+  return races.filter((race) => {
+    const startDate = new Date(race.start_date)?.toLocaleString("default", {
+      month: "long",
+    });
+    const endDate = new Date(race.end_date)?.toLocaleString("default", {
+      month: "long",
+    });
+    return months.includes(startDate) || months.includes(endDate);
+  });
+}
+
+// Takes 'races' (array of races (objects)) and 'categories' (array of categories (strings))
+function filterByCategories(races, categories) {
+  return races.filter((race) =>
+    race.engine_type.some((type) => categories.includes(type))
+  );
+}
+
+// Takes 'races' (array of races (objects)) and 'championships' (array of championships (strings))
+function filterByChampionships(races, championships) {
+  console.log(races);
+  return races.filter((race) =>
+    race.series.some((series) =>
+      championships.some((championship) =>
+        series.toLowerCase().includes(championship.toLowerCase())
+      )
+    )
+  );
+}
+
+// Takes 'races' (array of races (objects)) and 'countries' (array of countries (strings))
+function filterByCountries(races, countries) {
+  return races.filter((race) =>
+    countries.includes(race.circuits.countries.name)
+  );
+}
+
+// Takes 'races' (array of races (objects)) and 'sorting' (string containing 'ascending' or 'descending')
+// Returns sorted races by year as a map
+function sortIntoGroups(races, sorting) {
+  const groupedByYear = _.groupBy(races, (race) => race.end_date.slice(0, 4));
+
+  let sortedYears = Object.keys(groupedByYear).sort();
+  if (sorting?.includes("descending")) {
+    sortedYears = sortedYears.reverse();
+  }
+
+  const sortedGroupedByYear = new Map();
+  sortedYears.forEach((year) => {
+    sortedGroupedByYear.set(year, groupedByYear[year]);
+  });
+
+  return sortedGroupedByYear;
+}
+
 // Filter the races based on the filters selected
 // Returns 'filtered' (array of filtered races) and 'groupedByYear' (map of sorted races grouped by year)
 function applyFilters(races, filters) {
   let filtered = races;
 
-  if (filters.sorting?.length > 0) {
-    if (filters.sorting[0].includes("ascending")) {
-      filtered = filtered.sort(
-        (a, b) => new Date(a.end_date) - new Date(b.end_date)
-      );
-    } else if (filters.sorting[0].includes("descending")) {
-      filtered = filtered.sort(
-        (a, b) => new Date(b.end_date) - new Date(a.end_date)
-      );
-    }
-  }
+  if (filters.sorting?.length > 0)
+    filtered = sortRaces(filtered, filters.sorting[0]);
 
-  if (filters.status?.length > 0) {
-    const currentDate = new Date();
-    if (filters.status.includes("Upcoming")) {
-      filtered = filtered.filter(
-        (race) => new Date(race.end_date) > currentDate
-      );
-    } else if (filters.status.includes("Ongoing")) {
-      filtered = filtered.filter(
-        (race) =>
-          new Date(
-            race.start_date === null ? race.end_date : race.start_date
-          ) <= currentDate && new Date(race.end_date) >= currentDate
-      );
-    } else if (filters.status.includes("Finished")) {
-      filtered = filtered.filter(
-        (race) => new Date(race.end_date) < currentDate
-      );
-    }
-  }
+  if (filters.status?.length > 0)
+    filtered = filterByStatus(filtered, filters.status[0]);
 
-  if (filters.years?.length > 0) {
-    filtered = filtered.filter((race) =>
-      filters.years.includes(race.end_date.slice(0, 4))
-    );
-  }
+  if (filters.years?.length > 0)
+    filtered = filterByYears(filtered, filters.years);
 
-  if (filters.months?.length > 0) {
-    filtered = filtered.filter((race) => {
-      const startDate = new Date(race.start_date).toLocaleString("default", {
-        month: "long",
-      });
-      const endDate = new Date(race.end_date).toLocaleString("default", {
-        month: "long",
-      });
-      return (
-        filters.months.includes(startDate) || filters.months.includes(endDate)
-      );
-    });
-  }
+  if (filters.months?.length > 0)
+    filtered = filterByMonths(filtered, filters.months);
 
-  if (filters.categories?.length > 0) {
-    filtered = filtered.filter((race) =>
-      race.engine_type.some((type) => filters.categories.includes(type))
-    );
-  }
+  if (filters.categories?.length > 0)
+    filtered = filterByCategories(filtered, filters.categories);
 
-  if (filters.championships?.length > 0) {
-    filtered = filtered.filter((race) =>
-      race.series.some((series) =>
-        filters.championships.some((championship) =>
-          series.toLowerCase().includes(championship.toLowerCase())
-        )
-      )
-    );
-  }
+  if (filters.championships?.length > 0)
+    filtered = filterByChampionships(filtered, filters.championships);
 
-  if (filters.countries?.length > 0) {
-    filtered = filtered.filter((race) =>
-      filters.countries.includes(race.circuits.countries.name)
-    );
-  }
+  if (filters.countries?.length > 0)
+    filtered = filterByCountries(filtered, filters.countries);
 
-  // Sort races by year in ascending or descending order based on the sorting option
-  // If no sorting option is selected, sort races by year in ascending order
-  // Returns sorted races by year as a map
-  function sortIntoGroups(races, sorting) {
-    const groupedByYear = _.groupBy(races, (race) => race.end_date.slice(0, 4));
-    let sortedYears = Object.keys(groupedByYear).sort();
+  const groupedByYear = sortIntoGroups(filtered, filters.sorting[0]);
 
-    if (sorting?.length > 0 && sorting[0]?.includes("descending")) {
-      sortedYears = Object.keys(groupedByYear).sort().reverse();
-    }
-
-    const sortedGroupedByYear = new Map();
-    sortedYears.forEach((year) => {
-      sortedGroupedByYear.set(year, groupedByYear[year]);
-    });
-
-    return sortedGroupedByYear;
-  }
-
-  const groupedByYear = sortIntoGroups(filtered, filters.sorting);
   return { filtered, groupedByYear };
 }
 
